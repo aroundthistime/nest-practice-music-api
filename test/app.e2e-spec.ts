81fd8535cd9bc75644afc95dc434d17a644fc2d6
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2021-07-29 02:13:20 +0900

    e2e testin

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -12,6 +12,13 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true
+      })
+    )
     await app.init();
   });
 
@@ -19,6 +26,92 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my web site');
   });
+
+  describe("/music", () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get("/music")
+        .expect(200)
+        .expect([])
+    })
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post("/music")
+        .send({
+          title: "test song1",
+          artists: ["artist1", "artist2"]
+        })
+        .expect(201)
+    })
+    it('POST 202', () => {
+      return request(app.getHttpServer())
+        .post('/music')
+        .send({
+          title: "test song1"
+        })
+        .expect(400)
+    })
+    it('POST 203', () => {
+      return request(app.getHttpServer())
+        .post('/music')
+        .send({
+          artists: ["artist1", "artist2"]
+        })
+        .expect(400)
+    })
+    it('SEARCH title 200', () => {
+      return request(app.getHttpServer())
+        .get("/music/search?searchingby=test&method=title")
+        .expect(200)
+        .expect([])
+    })
+    it('SEARCH artist 200', () => {
+      return request(app.getHttpServer())
+        .get("/music/search?searchingby=test&method=artist")
+        .expect(200)
+        .expect([])
+    })
+    it('SEARCH 405', () => {
+      return request(app.getHttpServer())
+        .get("/music/search?searchingby=test&method=genre")
+        .expect(405)
+    })
+  })
+
+  describe("/music/:id", () => {
+    it("GET 200", () => {
+      return request(app.getHttpServer())
+        .get("/music/1")
+        .expect(200)
+    })
+    it("GET 404", () => {
+      return request(app.getHttpServer())
+        .get("/music/999")
+        .expect(404)
+    })
+    it("DELETE 200", () => {
+      return request(app.getHttpServer())
+        .delete("/music/1")
+        .expect(200)
+    })
+    it("DELETE 404", () => {
+      return request(app.getHttpServer())
+        .delete("/music/999")
+        .expect(404)
+    })
+    it("PATCH 200", () => {
+      return request(app.getHttpServer())
+        .patch("/movies/1")
+        .send({ title: "new title" })
+        .expect(200)
+    })
+    it("PATCH 404", () => {
+      return request(app.getHttpServer())
+        .patch("/movies/999")
+        .send({ title: "new title" })
+        .expect(404)
+    })
+  })
 });
